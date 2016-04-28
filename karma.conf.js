e1edba998a97d38690372c265a52f35bd6413732
Jovani Rico <jovanimtzrico@gmail.com>
2016-04-27 19:42:01 -0700

    Config Travis Test

--- karma.conf.js
+++ karma.conf.js
@@ -55,7 +55,17 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    // browsers: ['Chrome'],
+
+    browsers: ['ChromeExtra'],
+ 
+    // you can define custom flags 
+    customLaunchers: {
+      Chrome_without_security: {
+        base: 'Chrome',
+        flags: ['--disable-web-security']
+      }
+    }
 
 
     // Continuous Integration mode
