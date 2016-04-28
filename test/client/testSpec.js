c8bf8a369d06dd8313449cf59e65aa5d1a55c700
Jin Bok <devJin86@gmail.com>
2016-04-27 17:03:47 -0700

    Create a mock up test in $root/test/client/testSepc.js

---
+++ test/client/testSpec.js
@@ -0,0 +1,7 @@
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
+
+// describe("The 'toBe' matcher compares with ===", function() {
