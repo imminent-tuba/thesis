50480dacf07f6f578e1177bc1396bb2a197fdf54
Jovani Rico <jovanimtzrico@gmail.com>
2016-04-29 16:32:28 -0700

    AlchemyAPI

---
+++ server/models/analyzerModel.js
@@ -0,0 +1,6 @@
+// import db = from '../db/dbConfig.js';
+
+module.exports = {
+  saveAnalysis: (user,callback) => {
+  },
+};
--- server/server.js
+++ server/server.js
@@ -2,17 +2,21 @@ import http from 'http';
 import express from 'express';
 import io from 'socket.io';
 
+const routes = require('./config/routes.js');
+
 const app = express();
 const theServer = http.Server(app);
 const ioServer = io(theServer);
 
 const port = process.env.PORT || 1337;
 app.use(express.static(__dirname + '/../client'));
+routes(app);
 
 ioServer.on('connection', (socket) => {
   console.log('a user connected: ', socket);
 });
 
-let server = theServer.listen(port, () => {
+
+const server = theServer.listen(port, () => {
   console.log('listening on localhost:1337');
 });
