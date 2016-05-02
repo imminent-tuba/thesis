174a2047a3960cfc7f23fd7ff9d42fa1cff98969
Jin Bok <devJin86@gmail.com>
2016-05-02 11:53:31 -0700

    create a functional chatRoom with socket.io

--- client/Components/Chatroom.jsx
+++ client/Components/Chatroom.jsx
@@ -8,18 +8,41 @@ import TextField from 'material-ui/lib/text-field';
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
 
+/*
+this.props.chat [];
+events: sendChat()
+array: {user: '' , message: ''} user and Bot
+// on submit => props => sendChat()
+
+*/
 export default class Chatroom extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.setState({
+    //   allChats: this.props,
+    //
+    // })
+    // 1.redner all charts from this.props.chats
+    // 2. databinding between chats and textarea
+    //
+  }
   render() {
     return (
       <div>
         <List>
           <ListItem primaryText="test" />
         </List>
-        <TextField
-          fullwidth={true}
-          type="text"
-          floatingLabelText="Talk with the Bot!"
-        />
+        <div>
+          <form onSubmit={this.props.sendChat}>
+            <TextField
+              fullwidth={true}
+              type="text"
+              floatingLabelText="Talk with the Bot!"
+              value={this.props.chats}
+            />
+          <button> submit </button>
+          </form>
+        </div>
       </div>
     );
   }
