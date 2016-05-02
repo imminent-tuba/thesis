0316acd70d4f947b2dadad773debe78158691c8c
josh <jwentwor@gmail.com>
2016-05-02 11:05:42 -0700

    client socket connected

--- client/App.jsx
+++ client/App.jsx
@@ -1,36 +1,62 @@
 import React from 'react';
 import { render } from 'react-dom';
-import  { Grid, Row, Col } from 'react-flexbox-grid';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 /* React-tab-event-plugin */
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
-
-/* Socket.io */
-
 
 /* Import Components */
 import HeaderNavBar from './Components/HeaderNavBar.jsx';
 import DataViewList from './Components/DataViewList.jsx';
 import Chatroom from './Components/Chatroom.jsx';
 
-var App = () => (
-  <div>
-    <HeaderNavBar />
-    <Grid fluid>
-      <Row>
-        <Col md={3}>
-          <DataViewList />
-        </Col>
-        <Col md={6}>
-          <p> FOO </p>
-        </Col>
-        <Col md={3}>
-          <Chatroom />
-        </Col>
-      </Row>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chats: [],
+    };
+    socket.on('message', (msg) => {
+      const updateChat = this.state.chat.splice();
+      updateChat.push({
+        user: 'bot',
+        message: msg,
+      });
+      this.setState({ chats: updateChat });
+    });
+  }
 
-    </Grid>
-  </div>
-);
+  sendChat(msg) {
+    socket.emit('message', msg);
+    const updateChat = this.state.chat.splice();
+    updateChat.push({
+      user: 'user',
+      message: msg,
+    });
+    this.setState({ chats: updateChat });
+  }
+
+  render() {
+    return (
+      <div>
+        <HeaderNavBar />
+        <Grid fluid>
+          <Row>
+            <Col md={3}>
+              <DataViewList />
+            </Col>
+            <Col md={6}>
+              <p> FOO </p>
+            </Col>
+            <Col md={3}>
+              <Chatroom sendChat={this.sendChat} chats={this.state.chats} />
+            </Col>
+          </Row>
+
+        </Grid>
+      </div>
+    );
+  }
+}
 
 render(<App />, document.getElementById('app'));
