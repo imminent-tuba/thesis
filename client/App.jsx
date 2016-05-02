f390737d1a3cb8e0f1e8feb80f3b60d992aaef2f
Jin Bok <devJin86@gmail.com>
2016-05-02 10:04:47 -0700

    Merge/ pull

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import  { Grid, Row, Col } from 'react-flexbox-grid';
-/* Material Design module*/
-// import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
-// import MuiThemeProvider from 'material-ui/lib/MuiThemeProvider';
-/* End of Material Design module */
-
 /* React-tab-event-plugin */
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
-/* React-tab-event-plugin */
+
+/* Socket.io */
+
 
 /* Import Components */
 import HeaderNavBar from './Components/HeaderNavBar.jsx';
