495ebe7b438180e42adc15007ab5b371bd385799
Jin Bok <devJin86@gmail.com>
2016-05-03 13:41:37 -0700

    update .gitignore with DS_Store

--- karma.conf.js
+++ karma.conf.js
@@ -1,60 +1,67 @@
 // Karma configuration
 // Generated on Wed Apr 27 2016 16:41:03 GMT-0700 (PDT)
-// var webpack = require('webpack');
-// var webpackConfig = require('./webpack.config.js');
-// webpackConfig.entry = {};
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
+webpackConfig.entry = {};
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
+    // basePath: '',
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-
     // list of files / patterns to load in the browser
     files: [
-      'client/*.html' ,'test/**/*Spec.js'
+      'client/tests.webpack.js',
+      'server/tests/*',
     ],
-
 
     // list of files to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'client/tests.webpack.js': ['webpack', 'sourcemap'],
+      // preprocess with webpack and our sourcemap loader
     },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['spec'],
 
+    specReporter: {
+      maxLogLines: 5,
+      // limit number of lines logged per test
+      suppressErrorSummary: false,
+      // do not print error summary
+      suppressFailed: false,
+      // do not print information about failed tests
+      suppressPassed: false,
+      // do not print information about passed tests
+      suppressSkipped: true,
+      // do not print information about skipped tests
+      showSpecTiming: false,
+      // print the time elapsed for each spec
+    },
 
     // web server port
-    port: 9876,
-
+    // port: 9876,
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
@@ -63,19 +70,17 @@ module.exports = function(config) {
     customLaunchers: {
       Chrome_travis_ci: {
         base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
+        flags: ['--no-sandbox'],
+      },
     },
- 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
-
-    // webpack: webpackConfig
+    concurrency: Infinity,
+    webpack: webpackConfig,
   });
 };
--- test/client/testSpec.js
+++
@@ -1,8 +0,0 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    
-    expect(true).toBe(true);
-  });
-});
-
-// describe("The 'toBe' matcher compares with ===", function() {
