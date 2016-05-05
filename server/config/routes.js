865563611aa948f3d3143c69eb980af40d3c3173
Jin Bok <devJin86@gmail.com>
2016-05-05 01:10:23 -0700

    Include authy => two factor authenication via text/call/etc...

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,7 +5,8 @@ module.exports = (app) => {
   // app.get('/analysis', analyzerController.setAnalysis);
   app.get('/getanalysis', analyzerController.getAnalysis);
   app.post('/slack', slack.postMessage);
-  app.post('/login', Oauth2.login);
-  app.post('/signup', Oauth2.signup);
+  /* Login, Logout, Signup */
+  // app.post('/login', Oauth2.login);
+  // app.post('/signup', Oauth2.signup);
   // app.get('/', chatterBot)
 };
