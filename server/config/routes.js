f7a5b291961e9dfa8a71230df5ca19d5a48bb06a
Jin Bok <devJin86@gmail.com>
2016-05-06 12:08:23 -0700

    Change login path

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,13 +1,15 @@
 const analyzerController = require('../controllers/analyzerController.js');
 const slackBot = require('../controllers/slackController.js');
 const SlackOAuth2 = require('../controllers/oauth2Slack.js');
+/* Passort */
+const passort = require('passport');
 
 module.exports = (app) => {
   // app.get('/analysis', analyzerController.setAnalysis);
   app.get('/getanalysis', analyzerController.getAnalysis);
   app.post('/slackBot', slackBot.postMessage);
   /* Login, Logout, Signup */
-  app.post('/login/slack', SlackOAuth2.login); /* cb(req, res) */
+  app.post('/login', SlackOAuth2.login); /* cb(req, res) */
   // app.post('/signup', Oauth2.signup);
   // app.get('/', chatterBot)
 };
