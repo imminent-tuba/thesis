e041b71a320626dee4a05231cdb55ca4701e9f8d
Mark Tyneway <tyneway12@gmail.com>
2016-05-07 15:46:16 -0700

    Restructure files and implement headerbar links

--- client/Components/HeaderNavBar.jsx
+++ client/Components/HeaderNavBar.jsx
@@ -1,42 +1,33 @@
 import React from 'react';
+import FlatButton from 'material-ui/lib/flat-button';
 
-/* MUI lib files */
 import AppBar from 'material-ui/lib/app-bar';
-import IconButton from 'material-ui/lib/icon-button';
-import Avatar from 'material-ui/lib/avatar';
-import IconMenu from 'material-ui/lib/menus/icon-menu';
-import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
-import MenuItem from 'material-ui/lib/menus/menu-item';
 
-export default class HeaderNavBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <AppBar
-    title="Title"
-    iconElementLeft={
-      <IconButton>
-        <Avatar
-          src='https://lh5.ggpht.com/0WtsR4_NKcbftR5KvnlQ9YYpkBsfglEKhsvBbe-PSOgWkuDWIw2DIUnkmbuWy7DYFQ=w300-rw'
-          backgroundColor='Color.1A237E'
-          />
-      </IconButton>}
-    iconElementRight={
-      <IconMenu
-        iconButtonElement={
-          <IconButton><MoreVertIcon /></IconButton>
-        }
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-      >
-        <MenuItem primaryText="Refresh" />
-        <MenuItem primaryText="Help" />
-        <MenuItem primaryText="Sign out" />
-      </IconMenu>
-    }
-  />
-    )
-  }
-}
+const labelStyle = {
+  color: 'white',
+};
+
+const HeaderNavBar = () => (
+  <AppBar title="Charles the Chatterbot" showMenuIconButton={false}>
+    <FlatButton
+      href="#analytics"
+      linkButton={true}
+      label="Analytics"
+      labelStyle={labelStyle}
+    />
+    <FlatButton
+      href="#"
+      linkButton={true}
+      label="Home"
+      labelStyle={labelStyle}
+    />
+    <FlatButton
+      href="#about"
+      linkButton={true}
+      label="About"
+      labelStyle={labelStyle}
+    />
+  </AppBar>
+);
+
+export default HeaderNavBar;
--- client/entry.jsx
+++ client/entry.jsx
@@ -1,11 +1,8 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-// import SocketWrapper from './Components/SocketWrapper.jsx';
-// import RouterComponent from './Components/Router.jsx';
+
 import App from './Components/App.jsx';
 
-// render(<SocketWrapper />, document.getElementById('app'));
-render(<App />, document.getElementById('app'));
 
-// render the router here
+render(<App />, document.getElementById('app'));
