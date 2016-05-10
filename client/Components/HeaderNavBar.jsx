6935daeb7bf1a13bb7a22f00fcb65e8dc8c328ed
Jovani Rico <jovanimtzrico@gmail.com>
2016-05-09 20:16:45 -0700

    Adding Material UI

--- client/Components/HeaderNavBar.jsx
+++ client/Components/HeaderNavBar.jsx
@@ -26,9 +26,18 @@ const HeaderNavBar = () => (
       label="About"
       labelStyle={labelStyle}
     />
+<<<<<<< 014759250e365823359376541ccb2b712153345e
   <div>
     <a href="https://slack.com/oauth/authorize?scope=incoming-webhook,commands,bot&client_id=11495581584.40629389573&redirect_uri=http://uai.website/auth/slack/callback"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"/></a>
   </div>
+=======
+    <FlatButton
+      href="/auth/slack"
+      linkButton={true}
+      label="Login"
+      labelStyle={labelStyle}
+    />
+>>>>>>> Adding Material UI
   </AppBar>
 );
 
