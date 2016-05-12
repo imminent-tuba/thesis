b2df0ad0ed2b8d679682f073a17ac855c558eba9
Mark Tyneway <tyneway12@gmail.com>
2016-05-11 21:34:30 -0700

    Refactor analyzerController

---
+++ server/controllers/analysisController.js
@@ -0,0 +1,45 @@
+const alchemy = require('./alchemyController.js');
+const db = require('../models/databaseModel.js');
+const logger = require('../logger.js');
+
+module.exports = {
+  alchemyAnalyze: (message, callback) => {
+    const response = { message };
+    alchemy.getEmotions(message, (err, emotions) => {
+      response.emotions = emotions;
+      alchemy.getTaxonomy(message, (err, taxonomy) => {
+        response.taxonomy = taxonomy;
+        alchemy.getKeywords(message, (err, keywords) => {
+          response.keywords = keywords;
+          callback(null, response);
+        });
+      });
+    });
+  },
+  saveAnalysis: (analysis, callback) => {
+    db.saveMessage(analysis.message, (err, res) => {
+      logger.log('debug', 'Message saved', res);
+      db.saveEmotions(analysis.emotions, (err, res) => {
+        logger.log('debug', 'Emotions saved', res);
+        db.saveTaxonomy(analysis.taxonomy, (err, res) => {
+          logger.log('debug', 'Taxonomy saved', res);
+          callback(null, analysis);
+        });
+      });
+    });
+  },
+  getAnalysis: (data, callback) => {
+    const response = {};
+    db.getEmotions((err, emotions) => {
+      response.emotions = emotions;
+      db.getTaxonomy((err, taxonomy) => {
+        response.taxonomy = taxonomy;
+        db.getMessages(data, (err, messages) => {
+          response.messages = messages;
+          callback(null, response);
+        });
+      });
+    });
+  },
+};
+
